ea517a38500ede98d18971a9ee9f6e6136a5d85c
Leonid Verkhovtsev <leoromanovich@gmail.com>
2015-12-11 23:26:51 +1000

    End file form. JS begining

---
+++ app/js/my_work.js
@@ -0,0 +1,13 @@
+var myModule = (function () {
+    var init = function () {
+        _setUpListeners();
+    };
+    
+    var _setUpListeners = function () {
+        
+    }
+    return {
+        init: init
+    };
+})();
+myModule.init();
