cb76ead40f2c2d1ea2eeec92727f0c85a32a6874
Leonid Verkhovtsev <leoromanovich@gmail.com>
2015-12-13 17:21:22 +1000

    break form popup

--- app/js/my_work.js
+++ app/js/my_work.js
@@ -4,8 +4,41 @@ var myModule = (function () {
     };
     
     var _setUpListeners = function () {
+        $(".img_link").on('click', _showModal); // открыть модальное окно
+        $('#add_project').on('submit', _addProject); // добавление проекта
+    };
+    
+var _showModal = function(ev) {
+    console.log('Открыть модальное окно');
+    ev.preventDefault(); // Отменяем стандартное поведение ссылки
+    $('.hide_popup_block').bPopup({
+        speed: 650,
+        transition: "slideDown"
+    }); 
+};
+    
+    var _addProject = function(ev){
+        console.log('Добавление проекта');
+        ev.preventDefault();
+        
+        //Объявляем переменные
+        var form = $(this),// делаем из простого this jQuery объект
+            url = 'add_project.php',
+            data = form.serialize();
+        
+        console.log($('#add_input_name').val());
+        console.log(data);
+        // Ajax запрос на сервер
+        $.ajax ({
+            url: '',
+            type: "POST",
+            datatype: 'json',
+            data: data,
+        })
         
     }
+    
+    
     return {
         init: init
     };
